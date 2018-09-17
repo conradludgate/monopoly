684351c0144dd9eff94c245360f43b7d68a6a154
Conrad Ludgate <oon@conradludgate.com>
2018-09-17 16:56:14 +0100

    Update make clean, make websockets send to all connections. Websocket serverside connections can have state now, currently just a bool. A lot of work has been made to get not very far, but this allows for so much more now. React for front end. go for backend. protobuf for communication

--- Makefile
+++ Makefile
@@ -5,7 +5,6 @@ REACTDIR = react
 REACTSRCDIR = $(REACTDIR)/src
 REACTSRCS = $(wildcard $(REACTSRCDIR)/*)
 REACTBUILDDIR = $(REACTDIR)/build
-NODEMODULES = $(REACTDIR)/node_modules
 
 GOSRCDIR = server
 GOSRCS = $(wildcard $(GOSRCDIR)/*.go) $(PROTOSRC:%.proto=%.pb.go)
@@ -28,31 +27,25 @@ $(EXE): $(GOSRCS)
 $(GOSRCDIR)/bindata.go: $(REACTBUILDDIR)
 	go-bindata -o $(GOSRCDIR)/bindata.go $(BINDATADBG) -prefix $(REACTBUILDDIR)/ $(REACTBUILDDIR)/...
 
-$(PROTODIR)/%.go: $(PROTODIR)/%.proto
-	protoc -I=. -I=$$GOPATH/src -I=$$GOPATH/src/github.com/gogo/protobuf/protobuf --gogofaster_out=\
-Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,\
-Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,\
-Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,\
-Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,\
-Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:proto \
-proto/*.proto
+$(PROTODIR)/%.pb.go: $(PROTODIR)/%.proto
+	protoc -I=proto -I=$$GOPATH/src -I=$$GOPATH/src/github.com/gogo/protobuf/protobuf --gogofaster_out=\\
+Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,\\
+Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,\\
+Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,\\
+Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,\\
+Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:proto proto/*.proto
 
-$(REACTBUILDDIR): $(REACTSRCS) $(NODEMODULES) $(wildcard $(REACTDIR)/config/*) $(wildcard $(REACTDIR)/public/*) $(REACTSRCDIR)/protobuf.pb.js
-	cd $(REACTDIR) && \
-	yarn $(NODESCRIPT)
+$(REACTBUILDDIR): $(REACTSRCS) $(REACTDIR)/yarn.lock $(wildcard $(REACTDIR)/config/*) $(wildcard $(REACTDIR)/public/*) $(REACTSRCDIR)/protobuf.pb.js
+	cd $(REACTDIR) && yarn $(NODESCRIPT)
 
-$(NODEMODULES): $(REACTDIR)/package.json $(REACTDIR)/package-lock.json
-	cd $(REACTDIR) && \
-	yarn install
-
-# Do nothing. I just want it to be responsive to changes to the file
-$(REACTDIR)/package-lock.json:
+$(REACTDIR)/yarn.lock: $(REACTDIR)/package.json
+	cd $(REACTDIR) && yarn install
 
 $(REACTSRCDIR)/protobuf.pb.js: $(PROTOSRC)
 	pbjs -t static-module -w es6 $(PROTOSRC) -o $@
 
 clean:
-	rm -rf $(REACTBUILDDIR) $(EXE) $(GOSRCDIR)/bindata.go
+	rm -rf $(REACTBUILDDIR) $(EXE) $(GOSRCDIR)/bindata.go $(PROTOSRC:%.proto=%.pb.go) $(REACTSRCDIR)/protobuf.pb.js
 
 superclean: clean
 	rm -rf $(NODEMODULES)
--- server/ws.go
+++ server/ws.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -16,16 +17,35 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-// const (
-// 	// MessageType
-// 	ERROR = iota
-// 	CHAT
+var (
+	ErrorBadMessage = errors.New("ws: Client provided a bad websocket message")
+	ErrorBadProto   = errors.New("ws: Client provided a bad websocket protobuf")
+)
 
-// 	// ErrorType
-// 	INVALID      = iota
-// 	UNAUTHORIZED = 1
-// 	BAD          = 2
-// )
+func WSRead(conn *websocket.Conn) (*pb.Websocket, error) {
+	_, r, err := conn.NextReader()
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		if err := WSWriteError(conn, pb.Websocket_ErrorMessage_BAD, err.Error()); err != nil {
+			return nil, err
+		}
+		return nil, ErrorBadMessage
+	}
+
+	msg := &pb.Websocket{}
+	if err := msg.Unmarshal(b); err != nil {
+		if err := WSWriteError(conn, pb.Websocket_ErrorMessage_BAD, err.Error()); err != nil {
+			return nil, err
+		}
+		return nil, ErrorBadProto
+	}
+
+	return msg, nil
+}
 
 func WSWriteError(conn *websocket.Conn, typ pb.Websocket_ErrorMessage_ErrorType, msg string) error {
 	_msg := &pb.Websocket{
@@ -50,7 +70,6 @@ func WSWrite(conn *websocket.Conn, wsMsg *pb.Websocket) error {
 		return err
 	}
 
-	log.Println("Writing", wsMsg)
 	if _, err = w.Write(b); err != nil {
 		return err
 	}
@@ -61,47 +80,53 @@ func WSWrite(conn *websocket.Conn, wsMsg *pb.Websocket) error {
 	return nil
 }
 
+func WSWriteAll(wsMsg *pb.Websocket) error {
+	b, err := wsMsg.Marshal()
+	if err != nil {
+		return err
+	}
+
+	pm, err := websocket.NewPreparedMessage(websocket.BinaryMessage, b)
+	if err != nil {
+		return err
+	}
+
+	for conn, _ := range conns {
+		if err := conn.WritePreparedMessage(pm); err != nil {
+			conn.Close()
+		}
+	}
+
+	return nil
+}
+
+var conns = map[*websocket.Conn]bool{}
+
 func WSHandle(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		return
 	}
 
+	conns[conn] = true
+	defer func() {
+		delete(conns, conn)
+	}()
+
 	for {
-		_, r, err := conn.NextReader()
-		if err != nil {
-			return
-		}
-
-		b, err := ioutil.ReadAll(r)
-		if err != nil {
-			if err = WSWriteError(conn, pb.Websocket_ErrorMessage_BAD, err.Error()); err != nil {
-				log.Println(err.Error())
-				return
-			}
+		msg, err := WSRead(conn)
+		if err == ErrorBadMessage || err == ErrorBadProto {
 			continue
 		}
-
-		msg := &pb.Websocket{}
-		if err := msg.Unmarshal(b); err != nil {
-			if err = WSWriteError(conn, pb.Websocket_ErrorMessage_BAD, err.Error()); err != nil {
-				log.Println(err.Error())
-				return
-			}
-			continue
-		}
-
-		log.Println("Received", msg)
 
 		switch msg.GetType() {
 
 		case pb.Websocket_CHAT:
 			chat := msg.GetChat()
 			if chat != nil {
-				chat.User = "Server"
 				chat.Time = types.TimestampNow()
 
-				if err := WSWrite(conn, msg); err != nil {
+				if err := WSWriteAll(msg); err != nil {
 					log.Println(err.Error())
 					return
 				}
